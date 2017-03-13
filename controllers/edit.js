e847e28a83a676739959f6dbcee87163ea11fb49
Ench0 <ensar@farend.net>
2017-03-13 14:07:33 +0000

    upd

--- controllers/edit.js
+++ controllers/edit.js
@@ -130,6 +130,18 @@ exports.edit_post = function(req, res, next) {
         })
 
 
+        //restart
+        var sys = require('sys')
+        var exec = require('child_process').exec;
+        var child;
+        var sys = require('sys')
+        var exec = require('child_process').exec;
+        function puts(error, stdout, stderr) { sys.puts(stdout) }
+        exec("pm2 reload www", puts);
+
+
+
+
         res.redirect("/view/?message=Sucess!");
         // res.render('view', { title: 'View Timetable', settingsnew: settingsnew   , time: time, message: message });
 
