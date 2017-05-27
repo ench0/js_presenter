f1d118c4ec75f5c623e35b8d573db595cff205eb
Ench0 <ensar@farend.net>
2017-05-27 15:18:13 +0100

    upd

--- public/js/time.js
+++ public/js/time.js
@@ -181,8 +181,15 @@ var timeDisp = (function() {
     }
 	}
 
+  // Taraweeh Prayer
+  if ( (moment().format("iM") == '9') &&
+       (moment().isBetween(moment({hour: '23', minute: '00'}), moment({hour: '23', minute: '59'})))
+  ) {
+	  document.getElementById("overlay").style = "background:rgba(0,0,0,.85);z-index:1000;";
+	  document.getElementById("overlay").innerHTML = "Taraweeh Prayer<br/>"+moment().format('iDD iMMMM iYYYY')+"<br/>"+moment().format('DD MMMM YYYY');
+  }
   // Friday Prayer
-  if ( (moment().format("d") == '5') &&
+  else if ( (moment().format("d") == '5') &&
        (moment().isBetween(moment({hour: '13', minute: '10'}), moment({hour: '14', minute: '5'})))
   ) {
 	  document.getElementById("overlay").style = "background:rgba(0,0,0,.85);z-index:1000;";
