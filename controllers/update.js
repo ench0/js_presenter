2540eeaf5fccf5dee452250e05b334ed5bde0a7b
Ench0 <ensar@farend.net>
2017-06-12 17:59:56 +0100

    upd

--- controllers/update.js
+++ controllers/update.js
@@ -10,6 +10,8 @@ exports.github = function(req, res, next) {
   // var message = shell.echo('hello world');
   shell.exec('cd /srv/www/islamireland.ie/present');
   var message = shell.exec('git pull');
+  shell.exec('pm2 restart www');
+
   // var message = shell.exec('update-present');
   
   res.render('update', { title: 'Update', message: message });
