25dd64e95d9ccafa7b8e1a3fd4708ad5fc47f2f1
Ench0 <ensar@farend.net>
2017-06-12 17:54:12 +0100

    upd

--- controllers/update.js
+++ controllers/update.js
@@ -1,7 +1,7 @@
 // http://nodejs.org/api.html#_child_processes
 var sys = require('sys')
-var exec = require('child_process').exec;
-var util = require('util')
+// var exec = require('child_process').exec;
+// var util = require('util')
 var child;
 var shell = require('shelljs');
 
@@ -9,7 +9,7 @@ exports.github = function(req, res, next) {
 
   // var message = shell.echo('hello world');
   // var message = shell.exec('/Users/ensar/update-present');
-  var message = shell.exec('/home/icci/update-present');
+  var message = shell.exec('update-present');
   
   res.render('update', { title: 'Update', message: message });
 
