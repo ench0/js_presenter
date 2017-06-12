be487203564d06594b40d2c4d314203e95a5cb86
Ench0 <ensar@farend.net>
2017-06-12 18:01:01 +0100

    upd

--- controllers/update.js
+++ controllers/update.js
@@ -1,8 +1,3 @@
-// http://nodejs.org/api.html#_child_processes
-var sys = require('sys')
-// var exec = require('child_process').exec;
-// var util = require('util')
-var child;
 var shell = require('shelljs');
 
 exports.github = function(req, res, next) {
@@ -11,14 +6,6 @@ exports.github = function(req, res, next) {
   shell.exec('cd /srv/www/islamireland.ie/present');
   var message = shell.exec('git pull');
   shell.exec('pm2 restart www');
-
-  // var message = shell.exec('update-present');
   
   res.render('update', { title: 'Update', message: message });
-
-  // or more concisely
-  // var sys = require('sys')
-  // var exec = require('child_process').exec;
-  // function puts(error, stdout, stderr) { sys.puts(stdout) }
-  // exec("ls -la", puts);
 }
